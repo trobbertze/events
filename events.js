f4fed53f57a9a707fb1c53684addcc76898b5ad8
Theo Robbertze <theo.robbertze@gmail.com>
2014-07-18 09:58:29 +0200

    Notification status

--- events.js
+++ events.js
@@ -2,12 +2,13 @@ Events = function() {
   if ( arguments.callee._singletonInstance ) {
     return arguments.callee._singletonInstance;
   }
-
+  // ---------------------------------------------------------------------------
   _Events = function() {
     this.events = [];
   };
-
+  // ---------------------------------------------------------------------------
   _Events.prototype.constructor = Events;
+  // ---------------------------------------------------------------------------
   _Events.prototype.emit = function(name, obj) {
     _.each(this.events, function(evt){
       if(evt.name === name) {
@@ -15,12 +16,14 @@ Events = function() {
       }
     });
   };
+  // ---------------------------------------------------------------------------
   _Events.prototype.on = function(name, callback) {
     this.events.push({
       name: name,
       callback: callback
     });
   };
+  // ---------------------------------------------------------------------------
   arguments.callee._singletonInstance = new _Events();
 };
 
